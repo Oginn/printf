0ab09acb5f110f6f7e0e47c5ac14e34d30f8fa77
Oginn <oginniadeniyi@gmail.com>
2022-07-11 17:33:38 -0700

    task 0

--- main.h
+++ main.h
@@ -42,3 +42,11 @@ typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
+/* Funtions to print chars and strings */
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+#endif /* MAIN_H */
