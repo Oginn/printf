1f702362c82d433834d2cec409e5375012b7509c
Oginn <oginniadeniyi@gmail.com>
2022-07-11 18:09:58 -0700

    update main.h

--- main.h
+++ main.h
@@ -49,7 +49,19 @@ int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+/* Funciotns to handle other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
 
+/Function to print string in reverse/
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+/Function to print a string in rot 13/
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
 /* width_handler */
 int handle_write_char(char c, char buffer[],
